acebfdf6c4d4fc840cc830638f4c883aa275c0f3
David Thole <david@thedarktrumpet.com>
2021-11-11 20:11:05 -0600

    removing lint, adding run for the vue app itself

---
+++ server/frontend/src/components/ClientGraph.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="col-md-12">
+  <p>Clients Server Representation</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "ClientGraph"
+}
+</script>
+
+<style scoped>
+
+</style>
