577b7a01857156ffb3a42266dd5929fa7f0dc567
David Thole <david@thedarktrumpet.com>
2021-11-11 20:48:30 -0600

    Adding bootstrap and bootstrap-vue

--- server/frontend/src/main.js
+++ server/frontend/src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {BootstrapVue, IconsPlugin } from "bootstrap-vue"
+import { DropdownPlugin, TablePlugin, NavbarPlugin, LayoutPlugin } from 'bootstrap-vue'
+import { CardPlugin }  from 'bootstrap-vue'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(DropdownPlugin)
+Vue.use(TablePlugin)
+Vue.use(NavbarPlugin)
+Vue.use(LayoutPlugin)
+Vue.use(CardPlugin)
+Vue.use(VueResource)
 
 new Vue({
   render: h => h(App),
